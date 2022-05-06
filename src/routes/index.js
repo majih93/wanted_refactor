32af61f6d43d36fddc92637f69b0682dd94cd0cd
Jihyuk Ma <t90laser93@gmail.com>
2022-05-06 15:20:38 +0900

    build: 프로젝트 기본 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,7 @@
-import { LogoImage } from '../assets/svgs'
 import styles from './Routes.module.scss'
 
 function App() {
-  return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <LogoImage className={styles.appLogo} alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className={styles.appLink} href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div className={styles.app}>Hello</div>
 }
 
 export default App
