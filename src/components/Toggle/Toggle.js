92d6b193a6acfc085378e5bf2c88dd0b5e0dcbd4
Jihyuk Ma <t90laser93@gmail.com>
2022-05-07 18:03:37 +0900

    refactor(Toggle): Toggle 리팩토링 완료

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,7 +1,25 @@
 import styles from './Toggle.module.scss'
+import { cx } from '../../styles'
+import { useState } from 'react'
 
 function Toggle() {
-  return <div>Toggle</div>
+  const [toggle, setToggle] = useState(true)
+
+  const handleToggle = () => {
+    setToggle((prevState) => !prevState)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={cx(styles.toggleBar, { [styles.toggleRight]: !toggle })}> </div>
+      <button type='button' className={cx(styles.toggleItem, { [styles.toggled]: !toggle })} onClick={handleToggle}>
+        기본
+      </button>
+      <button type='button' className={cx(styles.toggleItem, { [styles.toggled]: toggle })} onClick={handleToggle}>
+        상세
+      </button>
+    </div>
+  )
 }
 
 export default Toggle
