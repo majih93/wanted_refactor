d59a97d7291060cf9626fbbb1064aedbb0baf0cd
Jihyuk Ma <t90laser93@gmail.com>
2022-05-08 15:59:34 +0900

    build: 과제 리팩토링 마무리 작업

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,20 @@
+import Input from '../components/Input/Input'
+import Slider from '../components/Slider/Slider'
+import Tab from '../components/Tab/Tab'
+import Toggle from '../components/Toggle/Toggle'
+import Dropdown from '../components/Dropdown/Dropdown'
 import styles from './Routes.module.scss'
 
 function App() {
-  return <div className={styles.app}>Hello</div>
+  return (
+    <div className={styles.app}>
+      <Toggle />
+      <Tab />
+      <Slider />
+      <Input />
+      <Dropdown />
+    </div>
+  )
 }
 
 export default App
